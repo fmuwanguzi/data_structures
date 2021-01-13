9c9b228886f23b6ae539bacfd1a49d7bb908634c
Felix Muwanguzi <felixm@Felixs-MacBook-Air.local>
2021-01-12 19:26:42 -0500

    reversing linked list

---
+++ linked_lists.js
@@ -0,0 +1,15 @@
+//reversing a linked list
+
+const reverseList = function(head){
+    let prev = null;
+    let curr = head; //current
+    let next = null;
+
+    while(curr){
+        next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+};
